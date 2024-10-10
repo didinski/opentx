206645cc712f3ddb6766b60ae67127bf7ec6a45e
didinski <131212467+didinski@users.noreply.github.com>
2024-10-10 10:34:43 +0200

    Update build-flysky.py

--- tools/build-flysky.py
+++ tools/build-flysky.py
@@ -21,7 +21,7 @@ boards = {
         "PCBI6X_INAV": "YES",
         "DFPLAYER": "YES",
     },
-    "I6X_RXASTX_PFET": {
+    "I6X_RXASTXPFET": {
         "HELI": "NO",
         "PCBI6X_ELRS": "YES",
         "PCBI6X_INAV": "YES",
